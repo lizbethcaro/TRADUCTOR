ad630fa1dd2738f2dc729024c1fa44f3c88b07c1
lizbethcaro <81368772+lizbethcaro@users.noreply.github.com>
2021-06-10 11:38:23 -0500

    orden de la base de datos
    se esta ordenando las carpetas de la base de datos con modelo/vista/controlador.

---
+++ 20210603-traductor/controllers/error.php
@@ -0,0 +1,11 @@
+<?php
+
+class Errores extends Controller{
+
+    function __construct(){
+        parent::__construct();
+        $this->view->mensaje = "Error al cargar el recurso";
+        $this->view->render('error/index');
+        //echo "<p>Error al cargar recurso</p>";
+    }
+}
---
+++ 20210603-traductor/libs/view.php
@@ -0,0 +1,11 @@
+<?php
+
+class View{
+    function __construct(){
+        echo "<p>vista base</p>";
+    }
+
+    function render($nombre){
+        require 'views/' . $nombre . '.php';
+    }
+}
